1392c193868185641b620c3d24c204ceb9d4330c
Albert Hovakimyan <HovakimyanAB@gmail.com>
2021-10-04 02:37:37 +0400

    new server py

--- client.py
+++ client.py
@@ -1,15 +1,14 @@
 import socket
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(("127.0.0.1", 12345))
+ClientSocket = socket.socket()
+host = '127.0.0.1'
+port = 1233
 
-client1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client1.connect(("127.0.0.1", 12346))
+
+print('Waiting for connection')
+ClientSocket.connect((host, port))
 
 
 while True:
-    data = client.recv(1024)
-    print(data.decode("utf-8"))
-    datax = client1.recv(1024)
-    t = datax.decode("utf-8")
-
+    Response = ClientSocket.recv(1024)
+    print(Response.decode('utf-8'))
--- server.py
+++ server.py
@@ -1,3 +1,38 @@
 import socket
+from _thread import *
+
+ServerSocket = socket.socket()
+host = '127.0.0.1'
+port = 1233
+ThreadCount = 0
+try:
+    ServerSocket.bind((host, port))
+except socket.error as e:
+    print(str(e))
+
+print('Waitiing for a Connection..')
+ServerSocket.listen(5)
 
 
+def threaded_client(connection):
+    welcome = 'Welcome to the Server'
+    connection.send(welcome.encode('utf-8'))
+    while True:
+        data = listcli[0].recv(1024)
+        reply = data.decode('utf-8')
+        print(reply, connection)
+        if not data:
+            pass
+        listcli[1].send(reply.encode("utf-8"))
+
+
+listcli=[]
+
+while True:
+    Client, address = ServerSocket.accept()
+    print('Connected to: ' + address[0] + ':' + str(address[1]))
+    listcli.append(Client)
+    print(Client)
+    start_new_thread(threaded_client, (Client, ))
+    ThreadCount += 1
+    print('Thread Number: ' + str(ThreadCount))
